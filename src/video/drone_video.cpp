7110ee064277ee1d73136ba68f3e8f0502502224
Ian McGaunn <ianmcgaunn@Ians-MacBook-Air.local>
2013-07-24 20:56:43 -0400

    added a useful thing to the imageconverter class

--- src/video/drone_video.cpp
+++ src/video/drone_video.cpp
@@ -10,48 +10,52 @@ using namespace std;
 static const char WINDOW[] = "Image window";
 
 class ImageConverter {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+    string name_ = "/ardrone/front/image_raw"
 
 public:
-  ImageConverter() : it_(nh_) {
-    /* subscribe to ardrone camera feed and publish it out as a new message */
-    image_pub_ = it_.advertise("converted", 1);
-    image_sub_ = it_.subscribe("/ardrone/front/image_raw", 1, &ImageConverter::imageCb, this);
+    /* constructor method called on instantiation of ImageConverter */
+    ImageConverter(string topic) : it_(nh_) {
+        /* subscribe to ardrone camera feed and publish it out as a new message */
+	name_ = topic;
+        image_pub_ = it_.advertise("converted", 1);
+        image_sub_ = it_.subscribe(string, 1, &ImageConverter::imageCb, this);
 
-    cv::namedWindow(WINDOW);
-  }
-
-  // destructor method called on destroy of ImageConverter instances
-  ~ImageConverter() {
-    cv::destroyWindow(WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr &msg) {
-    cv_bridge::CvImagePtr cv_ptr;
-    
-    try {
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }
-    catch (cv_bridge::Exception &e) {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+        cv::namedWindow(WINDOW);
     }
 
-    cv::imshow(WINDOW, cv_ptr -> image);
-    cv::waitKey(3);
+    // destructor method called on destroy of ImageConverter instances
+    ~ImageConverter() {
+        cv::destroyWindow(WINDOW);
+    }
 
-    image_pub_.publish(cv_ptr -> toImageMsg());
-  }
-  
+    /* takes an image_transport image and converts it to cvMat */
+    void imageCb(const sensor_msgs::ImageConstPtr &msg) {
+        cv_bridge::CvImagePtr cv_ptr;
+
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+        }
+        catch (cv_bridge::Exception &e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        cv::imshow(WINDOW, cv_ptr -> image);
+        cv::waitKey(3);
+
+        image_pub_.publish(cv_ptr -> toImageMsg());
+    }
+
 };
 
 int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic ;
-  cout << "This program is converting messages" << endl;
-  ros::spin();
-  return 0;
+    ros::init(argc, argv, "image_converter");
+    ImageConverter ic("/ardrone/front/image_raw") ;
+    cout << "This program is converting messages" << endl;
+    ros::spin();
+    return 0;
 }
