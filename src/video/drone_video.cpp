297122e67b2d8dcfa72001d869250a5e6ba82e8f
viki <viki@ROS.(none)>
2013-07-26 11:15:44 -0400

    made it not segfault when there are no targets

--- src/video/drone_video.cpp
+++ src/video/drone_video.cpp
@@ -24,14 +24,14 @@ class ImageConverter {
   unsigned int tags_count_;
   vector<unsigned int> tags_xc_;
   vector<unsigned int> tags_yc_;
-
+  
 public:
   /* constructor method called on instantiation of ImageConverter */
   ImageConverter(string topic) : it_(nh_) {
     /* subscribe to ardrone camera feed and publish it out as a new message */
     image_pub_ = it_.advertise("converted", 1);
-    image_sub_ = it_.subscribe(topic, 1, &ImageConverter::imageCb, this);
     navdata_sub_ = navdata_.subscribe("/ardrone/navdata", 10, &ImageConverter::navdataCallback, this);
+    image_sub_ = it_.subscribe(topic, 1, &ImageConverter::imageCb, this);
 
     cv::namedWindow(WINDOW);
   }
@@ -46,9 +46,13 @@ public:
     cv_bridge::CvImagePtr cv_ptr;
     cv::Point center;
     cv::Scalar color = cv::Scalar(14, 200, 60);
-    center.x = tags_xc_[0];
-    center.y = tags_yc_[0];
-    
+
+    /* make sure that it doesn't try to get this if there are no targets*/
+    if (tags_count_ == 1) {
+      center.x = tags_xc_[0] * .64;
+      center.y = tags_yc_[0] * .36;      
+    }
+
     try {
       cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
     }
@@ -56,8 +60,11 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-
-    cv::circle(cv_ptr -> image, center, 5, color, 2);
+    
+    /* this needs refactoring */
+    if (tags_count_ == 1) {
+      cv::circle(cv_ptr -> image, center, 5, color, 2);
+    }
     // cv::imshow(WINDOW, cv_ptr -> image);
     // cv::waitKey(3);
     image_pub_.publish(cv_ptr -> toImageMsg());
@@ -68,6 +75,8 @@ public:
     drone_state_ = msg -> state;
     tags_count_ = msg -> tags_count;
 
+    /* for the bottom camera this will be the round target
+       for the front camera these will be colored tags */
     tags_xc_ = msg -> tags_xc;
     tags_yc_ = msg -> tags_yc;
   }
@@ -75,7 +84,7 @@ public:
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "image_converter");
-  ImageConverter converter("/ardrone/front/image_raw");
+  ImageConverter converter("/ardrone/image_raw");
   cout << "This program is converting messages" << endl;
   ros::spin();
   return 0;
