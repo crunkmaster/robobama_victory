a7f50b718a014643d3f0e11d6156d03004af4fd0
viki <viki@ROS.(none)>
2013-08-05 09:29:21 -0400

    added python script for controlling

--- src/video/drone_video.cpp
+++ src/video/drone_video.cpp
@@ -24,19 +24,17 @@ class ImageConverter {
   unsigned int tags_count_;
   vector<unsigned int> tags_xc_;
   vector<unsigned int> tags_yc_;
-  
+
 public:
-  /* constructor method called on instantiation of ImageConverter */
   ImageConverter(string topic) : it_(nh_) {
     /* subscribe to ardrone camera feed and publish it out as a new message */
-    image_pub_ = it_.advertise("converted", 1);
+    image_pub_ = it_.advertise("tracked_targets", 1);
     navdata_sub_ = navdata_.subscribe("/ardrone/navdata", 10, &ImageConverter::navdataCallback, this);
     image_sub_ = it_.subscribe(topic, 1, &ImageConverter::imageCb, this);
 
     cv::namedWindow(WINDOW);
   }
 
-  // destructor method called on destroy of ImageConverter instances
   ~ImageConverter() {
     cv::destroyWindow(WINDOW);
   }
@@ -50,7 +48,7 @@ public:
     /* make sure that it doesn't try to get this if there are no targets*/
     if (tags_count_ == 1) {
       center.x = tags_xc_[0] * .64;
-      center.y = tags_yc_[0] * .36;      
+      center.y = tags_yc_[0] * .36;
     }
 
     try {
@@ -60,23 +58,16 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-    
     /* this needs refactoring */
     if (tags_count_ == 1) {
       cv::circle(cv_ptr -> image, center, 5, color, 2);
     }
-    // cv::imshow(WINDOW, cv_ptr -> image);
-    // cv::waitKey(3);
     image_pub_.publish(cv_ptr -> toImageMsg());
   }
-  
-  /* takes a navdata message and extracts useful things*/
+
   void navdataCallback(const ardrone_autonomy::Navdata::ConstPtr &msg) {
     drone_state_ = msg -> state;
     tags_count_ = msg -> tags_count;
-
-    /* for the bottom camera this will be the round target
-       for the front camera these will be colored tags */
     tags_xc_ = msg -> tags_xc;
     tags_yc_ = msg -> tags_yc;
   }
